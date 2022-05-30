333fb743ec2c01ee5ec6d26fbb0a4becf3a59126
Abhishek <94548108+ignabhishek@users.noreply.github.com>
2022-05-30 10:45:42 +0530

    CO3 Programs

--- CO3/Staff.java
+++ CO3/Staff.java
@@ -1,46 +1,75 @@
 import java.util.Scanner;
 
-class Employee {
+class Person {
+    String Name, Gender, Address;
+    int Age;
+
+    Person() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Person Name : ");
+        Name = sc.nextLine();
+        System.out.println("Enter Person Gender : ");
+        Gender = sc.nextLine();
+        System.out.println("Enter Person Address : ");
+        Address = sc.nextLine();
+        System.out.println("Enter Person Age : ");
+        Age = Integer.parseInt(sc.nextLine());
+
+    }
+}
+
+class Employee extends Person {
     int Empid;
-    String Name, Address;
+    String Company_Name, Qualification;
     double Salary;
 
-    Employee(){
+    Employee() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Employee Id : ");
+        System.out.println("Enter Employee ID : ");
         Empid = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter the Employee Name : ");
-        Name = sc.nextLine();
-        System.out.println("Enter the Employee Salary : ");
+        System.out.println("Enter Person Company Name : ");
+        Company_Name = sc.nextLine();
+        System.out.println("Enter Person Qualification : ");
+        Qualification = sc.nextLine();
+        System.out.println("Enter Employee Salary : ");
         Salary = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter the Employee Address : ");
-        Address = sc.nextLine();
+
     }
 }
 
 class Teacher extends Employee {
-    String Department;
-    String Subjects_taught;
+    String Subject, Department;
+    int TeacherId;
 
     Teacher() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Department : ");
+        System.out.println("Enter Teacher ID : ");
+        TeacherId = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter Teacher Departmenet : ");
         Department = sc.nextLine();
-        System.out.println("Enter the Subject ");
-        Subjects_taught = sc.nextLine();
+        System.out.println("Enter Teacher Subject : ");
+        Subject = sc.nextLine();
         System.out.println();
     }
 
     void Display() {
-        System.out.println("Employee Id : " +Empid);
-        System.out.println("Employee Name : " +Name);
-        System.out.println("Employee Salary : " +Salary);
-        System.out.println("Employee Address : " +Address);
-        System.out.println("Teacher Depaertment : " +Department);
-        System.out.println("Teacher Subjects : " +Subjects_taught);
+        System.out.println("Person Name : " + Name);
+        System.out.println("Person Gender : " + Gender);
+        System.out.println("Person Address : " + Address);
+        System.out.println("Person Age : " + Age);
+        System.out.println(" Employee ID : " + Empid);
+        System.out.println("Comapny Name : " + Company_Name);
+        System.out.println("Employee Qualification : " + Qualification);
+        System.out.println("Employee Salary : " + Salary);
+        System.out.println("Teacher ID : " + TeacherId);
+        System.out.println("Teacher Department : " + Department);
+        System.out.println("Teacher Subject : " + Subject);
         System.out.println();
+
+
     }
-} 
+
+}
 
 public class Staff {
     public static void main(String[] args) {
